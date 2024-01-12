b5ec2789375e1994b92e7c305c5418a02c57e83a
Aman Gairola <104727930+amngairola@users.noreply.github.com>
2024-01-12 21:16:19 +0530

    Update ObjectDestructring.js

--- 0-1/ObjectDestructring.js
+++ 0-1/ObjectDestructring.js
@@ -12,4 +12,8 @@ console.log(inst);
 
 //object destructting
 const navBar = ({ company }) => {};
-navBar({ company: "aman" });
+//JavaScript arrow function named navBar that takes an object as its parameter. The object has a property called company
+// ({ company }): This is the parameter list for the arrow function, and it uses object destructuring to 
+// extract the company property from the passed object.
+// It's equivalent to writing (props) => { const company = props.company; }, but in a more concise way.
+navBar({ company: "aman" }); //calling navBar with an object. 
